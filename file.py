a1fa253f589373612b8cd3ae466806e0ca04cf8b
kkl <2441389288@qq.com>
2022-08-17 14:23:38 +0800

    updated

---
+++ file.py
@@ -0,0 +1,30 @@
+import os,shutil
+
+def cd(target):
+    os.chdir(target)
+
+def dir():
+    return ','.join(os.listdir())
+
+def mkdir(dir_name):
+    os.mkdir(dir_name)
+
+def rm(dir_name):
+    if os.path.isfile(dir_name):
+        os.remove(dir_name)
+    elif os.path.isdir(dir_name):
+        shutil.rmtree(dir_name)
+
+def cat(file_path):
+    with open(file_path,'r',encoding='utf-8') as f:
+        str = f.read()
+        return str.rstrip()
+
+def copy(file_path,to_path):
+    shutil.copy(file_path,to_path)
+
+def move(file_path,to_path):
+    shutil.move(file_path,to_path)
+
+def rename(file_path,to_path):
+    os.rename(file_path,to_path)
